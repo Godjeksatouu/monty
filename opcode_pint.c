96c9cd68a57dfeddd6826ae4b2419afa3093c362
Godjeksatouu <godjeksatou@gmail.com>
2023-11-17 04:17:27 -0800

    pint push pall

---
+++ opcode_pint.c
@@ -0,0 +1,7 @@
+#include "monty.h"
+
+void opcode_pint(stack_t **stack, unsigned int line_number)
+{
+/* Implement the pint opcode logic here */
+}
+
---
+++ opcode_push.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+void opcode_push(stack_t **stack, unsigned int line_number)
+{
+/*Implement the push opcode logic here */
+}
+
+void opcode_pall(stack_t **stack, unsigned int line_number)
+{
+/*Implement the pall opcode logic here */
+}
+
